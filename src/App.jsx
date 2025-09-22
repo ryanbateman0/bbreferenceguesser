f75b8878448836ccb98e1dd26360563bd3350cd6
Ryan Bateman <ryan.bateman0@gmail.com>
2025-09-22 14:09:47 -0400

    added API calls and random images

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+async function getRandomImageURLFromAPI() {
+  const APIURL = 'https://xq6nvrkjrg.execute-api.us-east-1.amazonaws.com/test/RandomImageURL';
+  try {
+    const response = await fetch(APIURL);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
 
+    const result = await response.json();
+    return result.url;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function Image() {
+  const [imageURL, setImageURL] = useState(null);
+  
+  useEffect(() => {
+      getRandomImageURLFromAPI().then(url => setImageURL(url));
+  }, [])
+  
+  return (
+    <img src={imageURL}/>
+  )
+}
+
+function App() {
   return (
     <>
     <h1>Who dis?</h1>
-      <img
-      src='https://player-bb-reference-pages.s3.us-east-1.amazonaws.com/Jordan+BB+Reference.png'/>
+      <Image />
     </>
   )
 }
