476a62d79b8eb6b4cbdcc371b08215fae28a1a5c
Ryan Bateman <ryan.bateman0@gmail.com>
2025-11-15 01:10:25 -0500

    change assets directory

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './bbreferenceguesser/',
   build: {
     outDir: 'bbreferenceguesser',
+    assetsDir: 'bbreferenceguesser',
     emptyOutDir: true,
   }
 })
