471e87dda0d66404707d7ba08122c843b72cc631
Ryan Bateman <ryan.bateman0@gmail.com>
2025-11-15 01:18:34 -0500

    is this it

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'bbreferenceguesser',
     assetsDir: 'bbreferenceguesser',
     emptyOutDir: true,
   }
